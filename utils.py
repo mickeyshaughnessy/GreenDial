fed2d924daa8cff76571b3f79689c7a6476ad492
Mickey Shaughnessy <mickeyshaughnessy@Mickey-Shaughnessys-MacBook-Pro-M1.local>
2023-02-24 04:40:30 -0800

    rework bot interface

--- utils.py
+++ utils.py
@@ -7,8 +7,6 @@ The topics are the user's health, diet, sleep, activity, environment, mental and
 
 The chatbot can and usually does make calls to data translation and retrieval services, which are provided by a group of LLM API interfaces, generally called using capitalized symbols enclosed in angle brackets, like <SYMBOL>.
 
-The full documentation for the chatbot is available at https://github.com/mickeyshaughnessy/GreenDial/README.md
-
 In response to a question the chatbot should always provide a concise, correct answer.
 If the user indicates they're done chatting the chatbot should say good-bye.
 
@@ -24,10 +22,10 @@ The first response from the chatbot should be to ask for the user's name and pas
 A user response to a chatbot <AUTH> prompt is routed to an <AUTH> service, which replies with SUCCESS or FAIL.
 The AUTH service handles new users and always returns SUCCESS or FAIL. 
 
-<URD>
+<DRQ>
 %s
 
-<DRQ>
+<URD>
 %s
 
 Transcript:
@@ -55,62 +53,19 @@ URD_instructions = """
 When the user is answering a question from the chatbot or otherwise providing health data, the chatbot should include a redirect call to a data storage service, by the symbol <URD> in the bot response.
 For example:
 ---------
-Input Completion Prompt:
+Input:
 'User: I ate two apples yesterday.'
 'Bot:'
 
-Completion Output:
+ Output:
 'I recorded that you ate two apples yesterday <URD>. Did you eat anything else yesterday?'
 --------
+Input:
+'User: I went for a walk and drank my green giant drink'
+'Bot:'
+
+Output:
+'I recorded that you took a walk and drank the green giant drink <URD>.'
+
 """
 
-DRQ_instructions = """
-Data Retrieval Query <DRQ>
-The transcript below is bewtween a data-retrieval LLM API and a user.
-The user submits text queries about data they've previously entered into the API.
-The API responds with a concise, correct answer based on the data contained in the database query response given below:
-
-_content = %s
-
-LLM API response:
-"""
-
-URD_premamble = """
-User Response Data <URD>
-The input text below is a part of a conversation between a health assistant bot and a user.
-The input contains the bot's query and the user's response.
-This is an API service that takes the input and generates a well-formed json output string.
-The json output string is stored in a key-value database.
-The json output contains key value pairs like: 
-"sleep_start_time" : "9:00 PM"
- or
-"food_eaten" : {"name" : "Hashbrowns", "time" : "8:30 AM"}
-or
-"pill_taken" : {"name" : "GreenDial pill"}
-or
-"activity : {"name": "walk", "distance" : "2 miles", "start_name" : "Home", "end_name" : "Home"}
-
-The service extracts the unstructured input from the user and transforms it into the well-structured json output.
-In addition to producing the structured user data json object, the service also always appends contextual data through an external service call, using the special key-value pair <CONTEXT> : <CONTEXT>
-In cases in which the data provided in the user response component of the input is incomplete or insufficient to form a complete, well-formed and correct response the API service makes it's best attempt at an output string and also generates one of either:
-1. a prompt to the user for clarification, with the <FOLLOW> symbol like:
-  <FOLLOW> : "About what time did you take the pill?"
-  or
-  <FOLLOW> : "Were the ingredient in the hashbrowns just potatoes and some oil?"
-  or
-  <FOLLOW> : "About how long was the walk?"
-
-2. A query to an LLM database interface for the missing data with the <QUERY_DB> like:
-   <QUERY_DB> : "user_usual_wake_time"
-   or
-   <QUERY_DB> : "current_user_location"
-   or
-   <QUERY_DB> : "user_average_walking_distance"
-
-For missing data likely to already be in the database of health events for the user the service should use the <QUERY_DB> symbol to collect the missing data.
-For missing data unlikely to already be in the event database the service should use the <FOLLOW> symbol to prompt the user for the missing data.
- 
-Input = %
-
-JSON_OUTPUT = 
-"""
