99e32cd1a5a1eb0ae765a47b43cf167bef1e289c
Ubuntu <ubuntu@ip-172-31-54-121.ec2.internal>
2023-03-07 23:18:34 +0000

    https

--- api_server.py
+++ api_server.py
@@ -40,5 +40,5 @@ def chat():
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8011)
+    app.run(ssl_context='adhoc', debug=True, host='0.0.0.0', port=8011)
 
