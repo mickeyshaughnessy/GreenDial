0410c00f4e3895f20be3655c32f08aca42dd26ce
Ubuntu <ubuntu@ip-172-31-54-121.ec2.internal>
2023-03-16 04:01:45 +0000

    clean up webpage

--- chat_test.py
+++ chat_test.py
@@ -5,6 +5,6 @@ user_id = '123'
 
 while True:
     chat = input("User_%s: " % user_id)
-    resp = requests.post("http://127.0.0.1:8011/chat", json={"text" : chat, "user_id":user_id})
+    resp = requests.post("http://127.0.0.1:8012/chat", json={"text" : chat, "user_id":user_id})
     #resp = requests.post("http://3.89.10.143:8011/chat", json={"text" : chat, "user_id":user_id})
     print(resp.text)
